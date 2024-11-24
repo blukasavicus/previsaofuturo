135bbaf7e987f8edaff78f1e4f5304ccc835c06f
blukasavicus <bialukasavicus@gmail.com>
2024-11-24 20:22:58 -0300

    24/11

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.getElementById('learnMoreBtn').addEventListener('click', () => {
+    alert('Explore nossas seções para entender o impacto das mudanças climáticas e como você pode ajudar.');
+});
+
+document.getElementById('joinUsBtn').addEventListener('click', () => {
+    alert('Visite organizações ambientais locais e apoie projetos que ajudam a combater as mudanças climáticas!');
+});
